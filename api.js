6f3ff4cacf17ad8f88f4935f220bbd8c55f627a4
Trong Le <change1098@gmail.com>
2022-11-22 00:27:56 -0700

    v1.0 majority of code foundation added.

---
+++ api.js
@@ -0,0 +1,22 @@
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=eeadbda721fbc6bc30c5d27e76809db5', {
+  method: 'GET', //GET is the default.
+  credentials: 'same-origin', // include, *same-origin, omit
+  redirect: 'follow', // manual, *follow, error
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    drawWeather(data);
+  });
+
+  function drawWeather( d ) {
+    var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+    var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+    
+    document.getElementById('description').innerHTML = d.weather[0].description;
+    document.getElementById('temp').innerHTML = celcius + '&deg;';
+    document.getElementById('location').innerHTML = d.name;
+  }
+
